6f65ac516bedcef598a66aebfeb42162ce2ecb78
ivanErnestoDelgado <dino_fan2002@live.com.mx>
2025-11-29 13:29:23 -0700

    chore(project): add the dependencies and the required configs for firebase cloud messaging implementation in the Vitalis mobile app

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_core
+import firebase_messaging
 import mobile_scanner
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
 }
