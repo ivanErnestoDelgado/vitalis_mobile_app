8e920ef1825cfb68e8514a1bbfbda93fc0b7da2f
ivanErnestoDelgado <dino_fan2002@live.com.mx>
2025-11-29 13:36:22 -0700

    feat(data): add the method of the FCM token sending to the Django Vitalis Backend and inclute it in the login datasource method

--- lib/data/datasources/auth_remote_datasource.dart
+++ lib/data/datasources/auth_remote_datasource.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import '../models/auth_response.dart';
+import '../../utils/fcm_service.dart';
 
 class AuthRemoteDataSource {
   late final Dio _dio;
@@ -7,7 +8,7 @@ class AuthRemoteDataSource {
   AuthRemoteDataSource() {
     _dio = Dio(
       BaseOptions(
-        baseUrl: "http://127.0.0.1:8000/",
+        baseUrl: "http://192.168.100.61:8000/",
         headers: {"Content-Type": "application/json"},
         connectTimeout: const Duration(seconds: 8),
         receiveTimeout: const Duration(seconds: 8),
@@ -25,6 +26,7 @@ class AuthRemoteDataSource {
     );
   }
 
+  //función para iniciar sesión y manejar token FCM
   Future<AuthResponse> login(String email, String password) async {
     try {
       final response = await _dio.post(
@@ -32,7 +34,16 @@ class AuthRemoteDataSource {
         data: {"email": email, "password": password},
       );
 
-      return AuthResponse.fromJson(response.data);
+      final authResponse = AuthResponse.fromJson(response.data);
+
+      // obtener token FCM
+      final fcmToken = await FCMService.getToken();
+
+      if (fcmToken != null) {
+        await sendFcmToken(fcmToken, authResponse.accessToken);
+      }
+
+      return authResponse;
     } on DioException catch (e) {
       if (e.response != null) {
         throw Exception("Error: ${e.response?.data}");
@@ -42,6 +53,20 @@ class AuthRemoteDataSource {
     }
   }
 
+  //función para enviar el token FCM al backend
+  Future<void> sendFcmToken(String fcmToken, String authToken) async {
+    try {
+      await _dio.post(
+        "api/users/register-fcm-token/",
+        data: {"fcm_token": fcmToken},
+        options: Options(headers: {"Authorization": "Bearer $authToken"}),
+      );
+    } on DioException catch (e) {
+      print("Error al registrar FCM token: ${e.response?.data}");
+    }
+  }
+
+  //función para registrar un nuevo usuario
   Future<void> register({
     required String email,
     required String firstName,
@@ -58,7 +83,7 @@ class AuthRemoteDataSource {
           "last_name": lastName,
           "phone_number": phoneNumber,
           "password": password,
-          "role": "patient", // IMPORTANTE: siempre patient como pediste
+          "role": "patient",
         },
       );
     } on DioException catch (e) {
