214241af89b54fbabf87a99b822ae31d4df5f55f
ivanErnestoDelgado <dino_fan2002@live.com.mx>
2025-11-30 21:43:13 -0700

    feat(models): add the reminder access model for a later use of it in the reminder access features

---
+++ lib/data/models/reminder_access.dart
@@ -0,0 +1,38 @@
+// lib/data/models/reminder_access.dart
+import 'user_profile.dart';
+
+class ReminderAccess {
+  final int id;
+  final int reminder;
+  final int user;
+  final UserProfile userInfo;
+
+  final bool canEdit;
+  final bool canDelete;
+  final bool receiveNotifications;
+  final DateTime addedAt;
+
+  ReminderAccess({
+    required this.id,
+    required this.reminder,
+    required this.user,
+    required this.userInfo,
+    required this.canEdit,
+    required this.canDelete,
+    required this.receiveNotifications,
+    required this.addedAt,
+  });
+
+  factory ReminderAccess.fromJson(Map<String, dynamic> json) {
+    return ReminderAccess(
+      id: json["id"],
+      reminder: json["reminder"],
+      user: json["user"],
+      userInfo: UserProfile.fromJson(json["user_info"]),
+      canEdit: json["can_edit"],
+      canDelete: json["can_delete"],
+      receiveNotifications: json["receive_notifications"],
+      addedAt: DateTime.parse(json["added_at"]),
+    );
+  }
+}
